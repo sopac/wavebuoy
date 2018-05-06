8d288318d48aef731753405bb65b4b936dd6ea90
Sachindra Singh <sachindras@spc.int>
2018-05-06 18:02:35 +1200

    Dynamic charting implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,8 +24,10 @@ export class AppComponent implements OnInit {
 
   latest: LatestData[];
   waveData: WaveData[];
-  chart = new Array<Chart>();
+  chartMPP = new Array<Chart>();
   chartSWH = new Array<Chart>();
+  chartDIR = new Array<Chart>();
+  displayMax = "0";
   //testChart = TESTDATA;
 
 
@@ -71,18 +73,33 @@ export class AppComponent implements OnInit {
 
   }
 
+  chartRequest(max){
+    console.log("Request for : " + max);
+    this.processChart(this.waveData, max);
+  }
+
   getLatest(): void {
     this.service.getLatest().subscribe(latest => this.latest = latest);
   }
 
   getWaveData(): void {
-    this.service.getWaveData().subscribe(waveData => this.waveData = waveData, error1 => console.log("Error: ", error1), () => this.processChart(this.waveData));
+    this.service.getWaveData().subscribe(waveData => this.waveData = waveData, error1 => console.log("Error: ", error1), () => this.processChart(this.waveData, 50));
     //this.service.getWaveData().
   }
 
-  processChart(waveData1):void {
+  processChart(waveData1, max):void {
 
     console.log("processing chart..." + waveData1.data.spotterId  + " : " + waveData1.data.waves.length);
+
+    this.displayMax = max.toString();
+
+    //var max = 100;
+    var wavesArr = waveData1.data.waves.slice(waveData1.data.waves.length - max, waveData1.data.waves.length);
+    if (max == -1){
+     wavesArr = waveData1.data.waves;
+     max = waveData1.data.waves.length;
+     this.displayMax = max.toString();
+    }
 
     //swh
     this.chartSWH = new Array<Chart>();
@@ -90,9 +107,9 @@ export class AppComponent implements OnInit {
     let c0:Chart = new Chart();
     c0.name = "Significant Wave Height";
     var series:Series[] = new Array<Series>();
-    for (var i = 0; i < waveData1.data.waves.length; i++){
+    for (var i = 0; i < wavesArr.length; i++){
       let s:Series = new Series();
-      var wave:Wave = waveData1.data.waves[i];
+      var wave:Wave = wavesArr[i];
       s.name = wave.timestamp;
       s.value = wave.significantWaveHeight; //waveData1.data.waves[i].significantWaveHeight
       series.push(s);
@@ -100,8 +117,8 @@ export class AppComponent implements OnInit {
     c0.series = series;
     this.chartSWH.push(c0);
 
-    //all
-    this.chart = new Array<Chart>();
+    //mpp
+    this.chartMPP = new Array<Chart>();
     /*
     let c1:Chart = new Chart();
     c1.name = "Significant Wave Height";
@@ -120,35 +137,36 @@ export class AppComponent implements OnInit {
     let c2:Chart = new Chart();
     c2.name = "Peak Period";
     var series:Series[] = new Array<Series>();
-    for (var i = 0; i < waveData1.data.waves.length; i++){
+    for (var i = 0; i < wavesArr.length; i++){
       let s:Series = new Series();
-      var wave:Wave = waveData1.data.waves[i];
+      var wave:Wave = wavesArr[i];
       s.name = wave.timestamp;
       s.value = wave.peakPeriod; //waveData1.data.waves[i].significantWaveHeight
       series.push(s);
     }
     c2.series = series;
-    this.chart.push(c2);
+    this.chartMPP.push(c2);
 
     let c3:Chart = new Chart();
     c3.name = "Mean Period";
     var series:Series[] = new Array<Series>();
-    for (var i = 0; i < waveData1.data.waves.length; i++){
+    for (var i = 0; i < wavesArr.length; i++){
       let s:Series = new Series();
-      var wave:Wave = waveData1.data.waves[i];
+      var wave:Wave = wavesArr[i];
       s.name = wave.timestamp;
       s.value = wave.meanPeriod;
       series.push(s);
     }
     c3.series = series;
-    this.chart.push(c3);
+    this.chartMPP.push(c3);
 
+    /*
     let c4:Chart = new Chart();
     c4.name = "Peak Directional Spread";
     var series:Series[] = new Array<Series>();
-    for (var i = 0; i < waveData1.data.waves.length; i++){
+    for (var i = 0; i < wavesArr.length; i++){
       let s:Series = new Series();
-      var wave:Wave = waveData1.data.waves[i];
+      var wave:Wave = wavesArr[i];
       s.name = wave.timestamp;
       s.value = wave.peakDirectionalSpread
       series.push(s);
@@ -160,15 +178,46 @@ export class AppComponent implements OnInit {
     let c5:Chart = new Chart();
     c5.name = "Mean Directional Spread";
     var series:Series[] = new Array<Series>();
-    for (var i = 0; i < waveData1.data.waves.length; i++){
+    for (var i = 0; i < wavesArr.length; i++){
       let s:Series = new Series();
-      var wave:Wave = waveData1.data.waves[i];
+      var wave:Wave = wavesArr[i];
       s.name = wave.timestamp;
       s.value = wave.meanDirectionalSpread
       series.push(s);
     }
     c5.series = series;
     this.chart.push(c5);
+    */
+
+    //dir
+    this.chartDIR = new Array<Chart>();
+
+    let c6:Chart = new Chart();
+    c6.name = "Mean Direction";
+    var series:Series[] = new Array<Series>();
+    for (var i = 0; i < wavesArr.length; i++){
+      let s:Series = new Series();
+      var wave:Wave = wavesArr[i];
+      s.name = wave.timestamp;
+      s.value = wave.meanDirection;
+      series.push(s);
+    }
+    c6.series = series;
+    this.chartDIR.push(c6);
+
+    let c7:Chart = new Chart();
+    c7.name = "Peak Direction";
+    var series:Series[] = new Array<Series>();
+    for (var i = 0; i < wavesArr.length; i++){
+      let s:Series = new Series();
+      var wave:Wave = wavesArr[i];
+      s.name = wave.timestamp;
+      s.value = wave.peakDirection;
+      series.push(s);
+    }
+    c7.series = series;
+    this.chartDIR.push(c7);
+
 
 
 
